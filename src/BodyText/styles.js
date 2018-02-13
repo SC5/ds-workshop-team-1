1b50217704b13de85300c8b2f866997fe7ff948a
Timo Koro <timo.koro@sc5.io>
2018-02-13 16:51:28 +0200

    Changed body text font.

--- src/BodyText/styles.js
+++ src/BodyText/styles.js
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components'
 
 export const BodyText = styled.div`
-  font-family: lato, sans-serif;
+  font-family: Lobster, sans-serif;
   font-size: 1em;
   color: ${ props => props.color ? props.color : '#353740' };
   line-height: 1em;
