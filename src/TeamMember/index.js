564d122dabc0a31a2a2405d3d2fd1f48c1591d7a
Antti Salo <antti.salo@sc5.io>
2018-02-13 15:26:09 +0200

    TeamMember

--- src/TeamMember/index.js
+++ src/TeamMember/index.js
@@ -2,13 +2,29 @@ import React from 'react'
 
 import * as s from './styles.js'
 
+import Icon from '../Icon'
+
 const TeamMember = (props) => {
-  const { name, title, avatar } = props;
+  const { name, title, social, avatar } = props;
+
+  const socialItems = social ? Object.keys(social).map(key => {
+      let link = social[key]
+      if (key === 'twitter') {
+        link = `https://twitter.com/@${social[key]}`
+      }
+
+      return (<s.IconListItem>
+          <s.IconLink href={link} target="_blank"><Icon name={key}/></s.IconLink>
+        </s.IconListItem>)
+    }) : null
+  const socialList = social ? <s.IconList>{socialItems}</s.IconList> : null;
+
   return (
     <s.TeamMember>
-      <img src={ avatar } />
-      <s.Name>{ name }</s.Name>
-      <s.Title>{ title }</s.Title>
+      <img src={avatar} width="200" />
+      <s.Name>{name}</s.Name>
+      <s.Title>{title}</s.Title>
+      {socialList}
     </s.TeamMember>
   )
 }
