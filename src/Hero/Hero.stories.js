85e35da1ac810dd427cab4d99a02426f38b81221
Timo Koro <timo.koro@sc5.io>
2018-02-13 16:26:55 +0200

    Developed Hero component.

--- src/Hero/Hero.stories.js
+++ src/Hero/Hero.stories.js
@@ -3,9 +3,15 @@ import { storiesOf } from '@storybook/react'
 
 import Hero from '.'
 
+const logo = require('../../assets/dsconf-logo.png')
+
 storiesOf('Hero', module)
   .add('index', () => (
     <Hero
+      image={ logo }
       heading='Program'
+      subHeading='MARCH 14th – 15th, 2018 IN HELSINKI, FINLAND'
+      buttonLabel='GET TICKETS'
+      buttonHREF='http://google.com'
     />
   ))
--- src/Hero/styles.js
+++ src/Hero/styles.js
@@ -3,6 +3,13 @@ import styled, { css } from 'styled-components'
 export const Hero = styled.div`
   color: #fff;
   background-color: ${ props => props.bgColor ? props.bgColor : '#010733' };
+  display: flex;
+  align-items: center;
+  flex-direction: column;
   text-transform: uppercase;
   padding: 6rem 2rem;
+`
+
+export const Image = styled.img`
+  // padding: 6rem 2rem;
 `
