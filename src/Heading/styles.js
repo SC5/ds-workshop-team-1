997494c01d1bd0ad8c6fc579bf5c65382b6b3445
Timo Koro <timo.koro@sc5.io>
2018-02-13 16:40:17 +0200

    Increased Heading size.

--- src/Heading/styles.js
+++ src/Heading/styles.js
@@ -2,10 +2,9 @@ import styled, { css } from 'styled-components'
 
 export const Heading = styled.h1`
   font-family: ${ props => props.sub ? 'lato' : 'futura-pt-bold' }, sans-serif;
-  font-size: ${ props => props.sub ? '20px' : '30px' };
+  font-size: ${ props => props.sub ? '20px' : '70px' };
   color: ${ props => props.color ? props.color : '#353740' };
   line-height: 1.4em;
   margin: 0;
   text-align: ${ props => props.centered ? 'center' : 'left' };
-  text-transform: uppercase;
 `
